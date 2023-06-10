fd743d7dbb464b5c347442994b25702fd05808a1
arthur andrade <arthurandrad.23@gmail.com>
2023-06-10 10:41:41 -0300

    drawing done

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -5,16 +5,19 @@ import "./login.css"
 function Login() {
     return (
       <div>
-        <section className='companyTitle'>
-          <img className='companyLogo'/>
-          <p className='companyName'>Nutriscan</p>
-        </section>
-        <section className='login'>
-          <h1 className='welcomeTitle'>Bem vindo de volta</h1>
-          <h4>Entre na sua conta</h4>
-          <input placeholder='Email' type='text'/>
-          <input placeholder='Senha' type='text'/>
-          <button className='signInButton'>Entrar</button>
+        <section className='signupPage'>
+          <section className='companyTitle'>
+            <img src='https://img.freepik.com/free-icon/leaf_318-677961.jpg'
+            className='companyLogo'/>
+            <p className='companyName'>Nutriscan</p>
+          </section>
+          <section className='login'>
+            <h1 className='welcomeTitle'>Bem vindo de volta</h1>
+            <h4>Entre na sua conta</h4>
+            <input placeholder='Email' type='text'/>
+            <input placeholder='Senha' type='text'/>
+            <button className='signInButton'>Entrar</button>
+          </section>
         </section>
         <section className='signup'>
           <h1 className='newTitle'>Novo aqui?</h1>
