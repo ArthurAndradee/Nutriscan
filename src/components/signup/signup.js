ea617a86085f2725755f6eae23c015526c8a9950
arthur andrade <arthurandrad.23@gmail.com>
2023-06-17 10:10:10 -0300

    less gooo

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -6,33 +6,35 @@ function Signup() {
       <div>
         <section className='adPage'>
           <section className='adTextContainer'>
-            <h1 className='adTitle'>NutriScan</h1>
-            <h1 className='adSubTitle'>Crie uma conta</h1>
-            <article className='adAbout'>Faça parte da primeira plataforma aberta 
-              <br /> de informações nutricionais do Brasil.
+            <i className='adTitle'>NutriScan</i>
+            <h1 className='adSubTitle'>Crie uma conta!</h1>
+            <article className='adAbout'>Entre na maior plataforma 
+             de informações nutricionais do Brasil.
             </article>
           </section>
         </section>
         <section className='formContainer'>
-          <h1 className='formTitle'>Sign Up</h1>
-          <form>
-            <p className='inputTitle'>Nome completo</p>
-            <input placeholder='Seu nome aqui' type='password' pattern=".{8,}"
-            title="Insira no mínimo 8 caracteres" required
-            />
-            <p className='inputTitle'>Email</p>
-            <input placeholder='Seu email' type='email' 
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required
-            />
-            <p className='inputTitle'>Senha</p>
-            <input placeholder='sua senha' type='password' pattern=".{8,}"
-            title="Insira no mínimo 8 caracteres" required
-            />
-            <input type='submit' value='Criar conta'/>
-          </form>
-          <p className='alreadyAccountMember'>Já é um membro?
-            <p className='alreadyAccountMemberClick'>Entre aqui</p>
-          </p>
+          <section className='formBox'>
+            <h1 className='formTitle'>Sign Up</h1>
+            <form>
+              <p className='inputTitle'>Nome completo</p>
+              <input placeholder='Seu nome aqui' type='password' pattern=".{8,}"
+              title="Insira no mínimo 8 caracteres" required
+              />
+              <p className='inputTitle'>Email</p>
+              <input placeholder='Seu email' type='email' 
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required
+              />
+              <p className='inputTitle'>Senha</p>
+              <input placeholder='sua senha' type='password' pattern=".{8,}"
+              title="Insira no mínimo 8 caracteres" required
+              />
+              <input type='submit' value='Criar conta'/>
+            </form>
+            <p className='alreadyAccountMember'>Já é um membro?
+              <p className='alreadyAccountMemberClick'>Entre aqui</p>
+            </p>
+          </section>
         </section>
       </div>
     );
