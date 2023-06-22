c6d012948c1fbdfdd8db409c13813f0b516669aa
arthur andrade <arthurandrad.23@gmail.com>
2023-06-21 21:25:23 -0300

    fixinfg https

--- src/components/welcomepage/welcomepage.js
+++ src/components/welcomepage/welcomepage.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import "./welcomepage.css"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-    faPepperHot,
-    faAppleWhole,
-    faWheatAwn,
-    faBacon,
-    faShrimp,
-    faCheese,
-    faBurger,
-    faCakeCandles,
-    faQuestion,
-} from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { 
+//     faPepperHot,
+//     faAppleWhole,
+//     faWheatAwn,
+//     faBacon,
+//     faShrimp,
+//     faCheese,
+//     faBurger,
+//     faCakeCandles,
+//     faQuestion,
+// } from '@fortawesome/free-solid-svg-icons';
 
 function WelcomePage() {
     return (
       <div>
-        <FontAwesomeIcon icon={faPepperHot} style={{color: "#000000",}} />
+        {/* <FontAwesomeIcon icon={faPepperHot} style={{color: "#000000",}} />
         <FontAwesomeIcon icon={faAppleWhole} style={{color: "#000000",}} />
         <FontAwesomeIcon icon={faWheatAwn} style={{color: "#000000",}} />
         <FontAwesomeIcon icon={faBacon} style={{color: "#000000",}} />
@@ -25,7 +25,7 @@ function WelcomePage() {
         <FontAwesomeIcon icon={faCheese} style={{color: "#000000",}}/>
         <FontAwesomeIcon icon={faBurger} style={{color: "#000000",}}/>
         <FontAwesomeIcon icon={faCakeCandles} style={{color: "#000000",}}/>
-        <FontAwesomeIcon icon={faQuestion} style={{color: "#000000",}}/>
+        <FontAwesomeIcon icon={faQuestion} style={{color: "#000000",}}/> */}
       </div>
     );
   }
