ab4ce451b945d6324fd07b80e3d884fcbccaea7c
arthur andrade <arthurandrad.23@gmail.com>
2023-06-21 21:52:17 -0300

    html created

--- src/components/welcomepage/welcomepage.js
+++ src/components/welcomepage/welcomepage.js
@@ -1,31 +1,79 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react';
 import "./welcomepage.css"
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { 
-//     faPepperHot,
-//     faAppleWhole,
-//     faWheatAwn,
-//     faBacon,
-//     faShrimp,
-//     faCheese,
-//     faBurger,
-//     faCakeCandles,
-//     faQuestion,
-// } from '@fortawesome/free-solid-svg-icons';
 
 function WelcomePage() {
     return (
       <div>
-        {/* <FontAwesomeIcon icon={faPepperHot} style={{color: "#000000",}} />
-        <FontAwesomeIcon icon={faAppleWhole} style={{color: "#000000",}} />
-        <FontAwesomeIcon icon={faWheatAwn} style={{color: "#000000",}} />
-        <FontAwesomeIcon icon={faBacon} style={{color: "#000000",}} />
-        <FontAwesomeIcon icon={faShrimp} style={{color: "#000000",}} />
-        <FontAwesomeIcon icon={faCheese} style={{color: "#000000",}}/>
-        <FontAwesomeIcon icon={faBurger} style={{color: "#000000",}}/>
-        <FontAwesomeIcon icon={faCakeCandles} style={{color: "#000000",}}/>
-        <FontAwesomeIcon icon={faQuestion} style={{color: "#000000",}}/> */}
+        <header>
+          <div>
+            <img />
+            <h3 />
+          </div>
+          <div>
+            <div>
+              <h4>Carboidratos</h4>
+              <button></button>
+            </div>
+            <div>
+              <h4>Proteínas</h4>
+              <button></button>
+            </div>
+            <div>
+              <h4>Gorduras</h4>
+              <button></button>
+            </div>
+            <div>
+              <h4>Frutas</h4>
+              <button></button>
+            </div>
+          </div>
+          <div>
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </header>
+        <section>
+          <h1>Pesquisará o que hoje?</h1>
+          <div>
+            <input type='text' />
+          </div>
+          <div>
+            <button>Vegetais</button>
+            <button>Frutas</button>
+            <button>Carboidratos</button>
+            <button>Carnes</button>
+            <button>Frutos do Mar</button>
+            <button>Leite e Derivados</button>
+            <button>Fast Food</button>
+            <button>Açucarados</button>
+            <button>Em Dúvida</button>
+          </div>
+        </section>
+        <main>
+          <h2>Carnes Vermelhas</h2>
+          <div>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+          </div>
+        </main>
+        <main>
+          <h2>Carnes Brancas</h2>
+          <div>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+          </div>
+        </main>
       </div>
     );
   }
