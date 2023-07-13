2d87415bc6acd42eba016249c298b06a688ac648
arthur andrade <arthurandrad.23@gmail.com>
2023-07-13 15:40:36 -0300

    tutorials are live savers

--- src/components/welcomepage/welcomepage.js
+++ src/components/welcomepage/welcomepage.js
@@ -132,31 +132,61 @@ function WelcomePage() {
         </section>
           <h2 className='foodSectionTitle'>Carnes Vermelhas</h2>
         <div className='foodSection'>
-          <div className='foodBar'>
+          <div className='foodBar' id='foodBar-1'>
             <div className='foodImage'>AAA</div>
             <div className='foodImage'>AAA</div>
             <div className='foodImage'>AAA</div>
             <div className='foodImage'>AAA</div>
             <div className='foodImage'>AAA</div>
             <div className='foodImage'>AAA</div>
+            <a href='#foodBar-2'>
+              <svg>
+                <use href='#proximo'></use>
+              </svg>
+            </a>
           </div>
-          <div className='foodBar'>
+          <div className='foodBar' id='foodBar-2'>
+            <a href='#foodBar-1'>
+              <svg>
+                <use href='#anterior'></use>
+              </svg>
+            </a>
             <div className='foodImage'>BBB</div>
             <div className='foodImage'>BBB</div>
             <div className='foodImage'>BBB</div>
             <div className='foodImage'>BBB</div>
             <div className='foodImage'>BBB</div>
             <div className='foodImage'>BBB</div>
+            <a href='#foodBar-3'>
+              <svg>
+                <use href='#proximo'></use>
+              </svg>
+            </a>
           </div>
-          <div className='foodBar'>
+          <div className='foodBar' id='foodBar-3'>
+            <a href='#foodBar-2'>
+              <svg>
+                <use href='#anterior'></use>
+              </svg>
+            </a>
             <div className='foodImage'>CCC</div>
             <div className='foodImage'>CCC</div>
             <div className='foodImage'>CCC</div>
             <div className='foodImage'>CCC</div>
             <div className='foodImage'>CCC</div>
             <div className='foodImage'>CCC</div>
+            <a href='#foodBar-4'>
+              <svg>
+                <use href='#proximo'></use>
+              </svg>
+            </a>
           </div>
-          <div className='foodBar'>
+          <div className='foodBar' id='foodBar-4'>
+            <a href='#foodBar-3'>
+              <svg>
+                <use href='#anterior'></use>
+              </svg>
+            </a>
             <div className='foodImage'>DDD</div>
             <div className='foodImage'>DDD</div>
             <div className='foodImage'>DDD</div>
@@ -165,6 +195,17 @@ function WelcomePage() {
             <div className='foodImage'>DDD</div>
           </div>
         </div>
+        <svg>
+          <symbol id="proximo" viewBox="0 0 350 700">
+            <path fill="black"
+            d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" />
+          </symbol>
+          <symbol id="anterior" viewBox="0 0 350 700">
+            <path fill="black"
+            d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" />
+          </symbol>
+        </svg>
+
       </div>
     );
   }
